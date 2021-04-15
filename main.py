ecaa6713607c63a5ee04addca0e65c20c6f86974
Berrysoft <Strawberry_Str@hotmail.com>
2021-04-15 20:58:37 +0800

    Deal with triggers.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-import h5py
+import pandas as pd
 import math
 from zernike import RZern
 from scipy.special import legendre
@@ -24,33 +24,22 @@ with uproot.open(baseline_file) as ped:
 spe_file = ""
 
 spe_pre = wff.read_model(spe_file)
-opdt = np.dtype(
-    [
-        ("TriggerNo", np.uint32),
-        ("ChannelID", np.uint32),
-        ("HitPosInWindow", np.uint16),
-        ("Charge", np.float64),
-    ]
-)
 
 fipt = ""
-with h5py.File(fipt, "r", libver="latest", swmr=True) as ipt:
-    ent = ipt["Readout"]["Waveform"][()]
-    ent = ent[ent["ChannelID"] < 30]
-    l = len(ent)
-    print("{} waveforms will be computed".format(l))
-    assert np.all(pedcid == ent["ChannelID"]), "Files do not match!"
-    leng = len(ent[0]["Waveform"])
-    assert leng >= len(spe_pre[0]["spe"]), "Single PE too long which is {}".format(
-        len(spe_pre[0]["spe"])
-    )
+ent = pd.read_hdf(fipt, "Readout/Waveform")
+ent = ent[ent["ChannelID"] < 30]
+print("{} waveforms will be computed".format(len(ent)))
+assert np.all(pedcid == ent["ChannelID"]), "Files do not match!"
+leng = len(ent[0]["Waveform"])
+assert leng >= len(spe_pre[0]["spe"]), "Single PE too long which is {}".format(
+    len(spe_pre[0]["spe"])
+)
+
+ent = ent.groupby(by=["TriggerNo"])
 
 Thres = 0.1
 
 leng = len(ent[0]["Waveform"])
-dt = np.zeros(l * leng, dtype=opdt)
-start = 0
-end = 0
 
 nt = 80
 nr = 120
@@ -74,44 +63,52 @@ zs2 = np.array(
     [[[cart.Zk(v, r, theta) for r in rs2] for theta in thetas2] for v in range(nr)]
 )
 
-for i in range(l):
-    wave = (ent[i]["Waveform"] - pedestal[i]) * spe_pre[ent[i]["ChannelID"]]["epulse"]
-    A, wave, pet, mu, n = wff.initial_params(
-        wave, spe_pre[ent[i]["ChannelID"]], Thres, 4, 3
-    )
-    factor = np.linalg.norm(spe_pre[ent[i]["ChannelID"]]["spe"])
-    A = A / factor
-    gmu = spe_pre[ent[i]["ChannelID"]]["spe"].sum()
-    uniform_probe_pre = min(-1e-3 + 1, mu / len(pet))
-    probe_pre = np.repeat(uniform_probe_pre, len(pet))
-    (
-        xmmse,
-        xmmse_star,
-        psy_star,
-        nu_star,
-        T_star,
-        d_tot_i,
-        d_max,
-    ) = wff.fbmpr_fxn_reduced(
-        wave,
-        A,
-        probe_pre,
-        spe_pre[ent[i]["ChannelID"]]["std"] ** 2,
-        (40.0 * factor / gmu) ** 2,
-        factor,
-        20,
-        stop=0,
-    )
-    smmse = np.where(xmmse_star != 0, 1, 0)
-    pys = psy_star / np.prod(
-        np.where(smmse != 0, uniform_probe_pre, 1 - uniform_probe_pre), axis=1
-    )
-    ts2 = np.linspace(-1, 1, num=350 * n)
-    lt2 = np.array([legendre(v)(ts2) for v in range(nt)])
-    probe_func = np.exp(np.einsum("ij,ik,jlm->lmk", almn, lt2, zs2))
-    psv = np.prod(
-        np.einsum("ij,lmj->ilmj", smmse, probe_func)
-        + (1 - np.einsum("ij,lmj->ilmj", 1 - smmse, probe_func)),
-        axis=3,
-    )
-    total_psy = np.einsum("i,ilm->lm", pys, psv)
+for _, trig in ent:
+    total_psy = np.ones((100, 100))
+    for pe in trig.iloc:
+        # TODO: pedestal[i]
+        wave = (pe["Waveform"] - pedestal[i]) * spe_pre[pe["ChannelID"]]["epulse"]
+        A, wave, pet, mu, n = wff.initial_params(
+            wave, spe_pre[pe["ChannelID"]], Thres, 4, 3
+        )
+        factor = np.linalg.norm(spe_pre[pe["ChannelID"]]["spe"])
+        A = A / factor
+        gmu = spe_pre[pe["ChannelID"]]["spe"].sum()
+        uniform_probe_pre = min(-1e-3 + 1, mu / len(pet))
+        probe_pre = np.repeat(uniform_probe_pre, len(pet))
+        (
+            xmmse,
+            xmmse_star,
+            psy_star,
+            nu_star,
+            T_star,
+            d_tot_i,
+            d_max,
+        ) = wff.fbmpr_fxn_reduced(
+            wave,
+            A,
+            probe_pre,
+            spe_pre[pe["ChannelID"]]["std"] ** 2,
+            # TODO: 40.0: 单光电子响应的电荷分布展宽
+            (40.0 * factor / gmu) ** 2,
+            factor,
+            20,
+            stop=0,
+        )
+        smmse = np.where(xmmse_star != 0, 1, 0)
+        pys = psy_star / np.prod(
+            np.where(smmse != 0, uniform_probe_pre, 1 - uniform_probe_pre), axis=1
+        )
+        ts2 = (
+            np.concatenate([np.linspace(i, i + 1, num=(n + 1)) for i in pet]) / 175
+        ) - 1
+        lt2 = np.array([legendre(v)(ts2) for v in range(nt)])
+        probe_func = np.exp(np.einsum("ij,ik,jlm->lmk", almn, lt2, zs2))
+        psv = np.prod(
+            np.einsum("ij,lmj->ilmj", smmse, probe_func)
+            + (1 - np.einsum("ij,lmj->ilmj", 1 - smmse, probe_func)),
+            axis=3,
+        )
+        total_psy *= np.einsum("i,ilm->lm", pys, psv)
+    ind = np.unravel_index(np.argmax(total_psy), total_psy.shape)
+    print("({}, {})".format(rs2[ind[1]], thetas2[ind[0]]))
